7b9b6107db39c6d4c6083c7807f5ad29dfb4a7a7
P-a-u-l-o <paulo.ads.silva@hotmail.com>
2022-12-13 13:45:53 -0300

    Adição de validação do campo do formulário antes da inserçao do valor na listagem.

--- script.js
+++ script.js
@@ -1,91 +1,81 @@
-const wrapperLinguagens = document.querySelector('.wrapper-linguagens');
+const wrapperLinguagens = document.querySelector(".wrapper-linguagens");
 const listaDeLinguagens = [];
 let incrementoLista = 0;
 
 const renderizaParagrafoListaVazia = () => {
-   wrapperLinguagens.innerHTML = `
+	wrapperLinguagens.innerHTML = `
         <h2>Lista de Linguagens:</h2>
-        <p class="lista-vazia">Não existem elementos nessa lista</p>   
+        <p class="lista-vazia">Não existem elementos nessa lista</p>
     `;
-}
+};
 
-const renderizaLista = () => {   
-    wrapperLinguagens.innerHTML = `
+const renderizaLista = () => {
+	wrapperLinguagens.innerHTML = `
         <h2>Lista de Linguagens:</h2>
-        <ul class="lista-linguagens">                    
-        </ul>  
+        <ul class="lista-linguagens">
+        </ul>
     `;
 
-    incrementoLista = 0;
+	incrementoLista = 0;
 
-    const ul = document.querySelector('.lista-linguagens');
-    listaDeLinguagens.forEach((linguagem) => {
-        const itemLista = criarItem(linguagem.valor);
-        linguagem.valor = itemLista.valor;
-        linguagem.idItem = itemLista.idItem;
-        linguagem.itemElement = itemLista.itemElement;
-        ul.appendChild(linguagem.itemElement);
-    });
-}
-
+	const ul = document.querySelector(".lista-linguagens");
+	listaDeLinguagens.forEach((linguagem) => {
+		const itemLista = criarItem(linguagem.valor);
+		linguagem.valor = itemLista.valor;
+		linguagem.idItem = itemLista.idItem;
+		linguagem.itemElement = itemLista.itemElement;
+		ul.appendChild(linguagem.itemElement);
+	});
+};
 
 const removerItem = (idItem) => {
-    const linguagemParaRemover = document.querySelector(`#${idItem}`).innerText;
-    const indexParaRemover = listaDeLinguagens.findIndex((e) => e.valor === linguagemParaRemover);
-    listaDeLinguagens.splice(indexParaRemover, 1);
+	const linguagemParaRemover = document.querySelector(`#${idItem}`).innerText;
+	const indexParaRemover = listaDeLinguagens.findIndex(
+		(e) => e.valor === linguagemParaRemover
+	);
+	listaDeLinguagens.splice(indexParaRemover, 1);
 
-    if(!listaDeLinguagens.length)
-        renderizaParagrafoListaVazia();
-    else
-        renderizaLista();
-}
-
-
+	if (!listaDeLinguagens.length) renderizaParagrafoListaVazia();
+	else renderizaLista();
+};
 
 const criarElementoRemovedor = (idItem) => {
-    const removedor = document.createElement('input');
-    removedor.setAttribute("type", "button");
-    removedor.value ='(X)';
-    removedor.addEventListener('click', () => removerItem(idItem));
-    return removedor;
-}
+	const removedor = document.createElement("input");
+	removedor.setAttribute("type", "button");
+	removedor.value = "(X)";
+	removedor.addEventListener("click", () => removerItem(idItem));
+	return removedor;
+};
 
+const criarItem = (valor) => {
+	const idItem = `item-${incrementoLista++}`;
+	const itemElement = document.createElement("li");
+	itemElement.innerText = valor;
+	itemElement.setAttribute("id", idItem);
+	const removedorItem = criarElementoRemovedor(idItem);
+	itemElement.appendChild(removedorItem);
 
-
-
-const criarItem = (valor) => {  
-    const idItem = `item-${incrementoLista++}`;
-    const itemElement = document.createElement('li');
-    itemElement.innerText = valor;
-    itemElement.setAttribute('id', idItem);
-    const removedorItem = criarElementoRemovedor(idItem);
-    itemElement.appendChild(removedorItem);
-
-    return {valor, idItem, itemElement};
-}
-
-
+	return { valor, idItem, itemElement };
+};
 
 const addElement = (event) => {
-    event.preventDefault();
+	event.preventDefault();
 
-    //Captura o valor
-    const valor = event.target.elements[0].value;
+	//Captura o valor
+	const valor = event.target.elements[0].value;
 
-    // Limpa campo 
-    event.target.elements[0].value = "";
+	// Limpa campo
+	event.target.elements[0].value = "";
 
-    //Cria o item com seu respectivo removedor
-    const itemLista = criarItem(valor);
-    listaDeLinguagens.push(itemLista);
-
-    renderizaLista();
-}
-
-
+	//Realiza a verificação do campo do formulário se está vazio ou não e cria o item com seu respectivo removedor.
+	const itemLista = criarItem(valor);
+	if (valor == "") {
+		alert("O campo (Linguagem) é de preenchimento obrigatório!");
+	} else {
+		listaDeLinguagens.push(itemLista);
+		renderizaLista();
+	}
+};
 
 const elementoForm = document.querySelector("form");
-elementoForm.addEventListener('submit', addElement);
-
-
-
+elementoForm.addEventListener("submit", addElement);
